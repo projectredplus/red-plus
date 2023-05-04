d98511913126f235a557f2510344167ab9245760
Marcos Airton <marcosmacedo312@gmail.com>
2023-05-04 12:30:28 -0300

    feature(): plano de fundo e logo

--- screens/home/index.jsx
+++ screens/home/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Box, Text } from "native-base";
+import { Box, Text, Image } from "native-base";
 import { styles } from "./styles";
+import bg from "../../assets/images/bg.png";
+import logo from "../../assets/images/logo.png";
 
 export const HomeRoute = "Home";
 
 export function Home() {
   return (
     <Box {...styles.container}>
-      <Text>Início</Text>
+      <Image {...styles.bg} source={bg} />
+      <Image {...styles.logo} source={logo} />
     </Box>
   );
 }
--- screens/home/styles.js
+++ screens/home/styles.js
@@ -1,5 +1,16 @@
 export const styles = {
   container: {
     flex: 1,
+    position: "relative",
+  },
+  bg: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    zIndex: 0,
+  },
+  logo: {
+    width: "300px",
+    height: "300px",
   }
 }
