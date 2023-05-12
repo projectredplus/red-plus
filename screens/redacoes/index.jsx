e09e0fa584fad8f670ca87d71e0abd506acfbe2d
ligiaevellin <l.evellin@hotmail.com>
2023-05-12 19:02:03 -0300

    msg

--- screens/redacoes/index.jsx
+++ screens/redacoes/index.jsx
@@ -1,13 +1,41 @@
-import React from "react";
-import { Box, Text } from "native-base";
+import { Box, Text, Button, View, Center, Card, VStack, Heading } from "native-base";
 import { styles } from "./styles";
+import fundo from "../../assets/images/fundo.png";
+import { Image } from "react-native";
 
 export const RedacoesRoute = "Redações nota 1000";
 
+function RedacaoCard() {
+  const cardStyle = {
+    backgroundColor: "transparent",
+    ...styles.card,
+  };
+
+  const backgroundImageStyle = {
+    flex: 1,
+    resizeMode: "cover",
+    ...styles.backgroundImage,
+  };
+
+  return (
+    <Card width="50%" my={4} p={4} style={cardStyle}>
+      <View style={backgroundImageStyle}>
+        <Image source={fundo} style={styles.image} />
+      </View>
+      <VStack space={4} style={styles.content}>
+        <Heading size="md" style={styles.redacaoTitle}>REDAÇÃO DE BÁRBARA</Heading>
+        <Button bg={"green.800"} onPress={() => console.log("hello world")} style={styles.button}>
+          Conferir Redação
+        </Button>
+      </VStack>
+    </Card>
+  );
+}
+
 export function Redacoes() {
   return (
-    <Box {...styles.container}>
-      <Text>Redações</Text>
+    <Box alignItems="center">
+      <RedacaoCard />
     </Box>
   );
 }
--- screens/redacoes/styles.js
+++ screens/redacoes/styles.js
@@ -1,5 +1,32 @@
 export const styles = {
-  container: {
+  card: {
+    overflow: "hidden",
+  },
+  backgroundImage: {
+    position: "absolute",
+    top: -50,
+    left: 0,
+    right: 0,
+    bottom: -50,
+    zIndex: -1,
+    
+  },
+  image: {
     flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  content: {
+    padding: 16,
+  },
+  redacaoTitle: {
+    fontSize: 12,
+    fontWeight: "bold",
+    top: 2,
+    left: 29,
+  },
+  button: {
+    left: 13,
+    top: 40,
   }
-}
+};
