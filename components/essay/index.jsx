a221ab81e27933b222399e5f43c2143ceecb5dbe
victor-renan <alvesrenanpro@gmail.com>
2023-05-14 00:08:22 -0300

    fix(): redacoes em duas colunas

--- components/essay/index.jsx
+++ components/essay/index.jsx
@@ -1,25 +1,19 @@
 import React from "react";
-import { Box, Button, Center, Heading, Image, VStack, Text, HStack, theme } from "native-base";
+import { Box, Button, Center, Heading, Image, VStack, Text, HStack, theme, View } from "native-base";
 import notebook from "../../assets/images/notebook.png";
-import Icon  from "react-native-vector-icons/Ionicons";
+import Icon from "react-native-vector-icons/Ionicons";
 
 
-export function Essay({ tema, autor, arquivo }) {
+export function Essay({ autor, arquivo }) {
   return (
     <React.Fragment>
-      <VStack bg={"white"} borderWidth={1} overflow={"hidden"} borderRadius={8} borderColor={"gray.300"} w={"100%"}>
-        <Center padding={5} background={"green.200"}>
+      <VStack bg={"white"} borderWidth={1} overflow={"hidden"} borderRadius={8} borderColor={"gray.300"} w={"50%"}>
+        <Center padding={5} py={8} pb={6} background={"green.200"}>
           <Image size={"100px"} source={notebook} alt="Imagem de um caderno sorrindo" />
         </Center>
-        <VStack padding={3}>
-          <Heading fontSize={22}>{tema}</Heading>
-          <HStack mt={"26px"}>
-            <Text fontSize={17}>{autor}</Text>
-            <HStack ml={"auto"} alignItems={"center"}>
-              <Icon name="star" size={20} color={"orange"} />
-              <Text ml={1} fontSize={17}>1000</Text>
-            </HStack>
-          </HStack>
+        <VStack alignItems={"center"} bg="green.200">
+          <Text fontSize={18}>Redação de</Text>
+          <Text fontSize={20} bold>{autor}</Text>
         </VStack>
         <Button size={"lg"} borderRadius={0} colorScheme={"green"}>Ver redação</Button>
       </VStack>
--- screens/redacoes/index.jsx
+++ screens/redacoes/index.jsx
@@ -1,19 +1,46 @@
 import React from "react";
-import { Box, Text, Button, View, Center, Card, VStack, Heading } from "native-base";
+import { Box, Text, Button, View, Center, Card, VStack, Heading, ScrollView } from "native-base";
 import { styles } from "./styles";
 import { Essay } from "../../components/essay";
 
 export const RedacoesRoute = "Redações nota 1000";
 
+const essays = [
+  {
+    tema: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, beatae?",
+    autor: "Renan Alves",
+    file: "google.com"
+  },
+  {
+    tema: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, beatae?",
+    autor: "Lorem ipsum",
+    file: "google.com"
+  },
+  {
+    tema: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, beatae?",
+    autor: "Lorem ipsum",
+    file: "google.com"
+  },
+  {
+    tema: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, beatae?",
+    autor: "Lorem ipsum",
+    file: "google.com"
+  }
+]
+
 export function Redacoes() {
   return (
     <React.Fragment>
-      <Box padding={4} {...styles.container}>
-        <Essay
-          tema={"Desafios para a formação educacional de surdos no Brasil"}
-          autor={"Renan Alves"}
-        />
-      </Box>
+      <ScrollView mb={3} flex={1} padding={4} {...styles.container}>
+        {essays.map(essay => (
+          <Essay
+            key={essay.file}
+            tema={essay.tema}
+            autor={essay.autor}
+            arquivo={essay.file}
+          />
+        ))}
+      </ScrollView>
     </React.Fragment>
 
   );
